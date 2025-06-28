d880e3b46ce579f1fc19975b5af4d951c8fef1c0
manhtranduc <manhtranduc0202@gmail.com>
2025-06-28 16:19:48 +0700

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Welcome to My App</h1>
+      <p>This is the home page of my application.</p>
+      <p>Feel free to explore the features and functionalities.</p>
+      <p>Enjoy your stay!</p>
+    </div>
+  );
+}
